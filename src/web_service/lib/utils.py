ab438d1773dff718aa22c7bb24e5ca6a7c10ce10
JACK <jackkhoueiri1@gmail.com>
2024-10-24 18:31:44 +0200

    utils.py

---
+++ src/web_service/lib/utils.py
@@ -0,0 +1,21 @@
+# Utils file. TODO: add a `load_object` function to load pickle objects
+
+import os
+import pickle
+from functools import lru_cache
+
+from loguru import logger
+
+
+@lru_cache
+def load_preprocessor(filepath: os.PathLike):
+    logger.info(f"Loading preprocessor from {filepath}")
+    with open(filepath, "rb") as f:
+        return pickle.load(f)
+
+
+@lru_cache
+def load_model(filepath: os.PathLike):
+    logger.info(f"Loading model from {filepath}")
+    with open(filepath, "rb") as f:
+        return pickle.load(f)
