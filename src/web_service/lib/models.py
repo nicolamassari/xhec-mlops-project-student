d5b70f1eacc46c8e3a9eaa36afbb8c656f58892b
JACK <jackkhoueiri1@gmail.com>
2024-10-24 18:29:55 +0200

    models.py

--- src/web_service/lib/models.py
+++ src/web_service/lib/models.py
@@ -1,1 +1,17 @@
 # Pydantic models for the web service
+
+from pydantic import BaseModel
+
+class InputData(BaseModel):
+    length: float
+    diameter: float
+    height: float
+    whole_weight: float
+    shucked_weight: float
+    viscera_weight: float
+    shell_weight: float
+    sex: str
+
+
+class PredictionOut(BaseModel):
+    rings: float
