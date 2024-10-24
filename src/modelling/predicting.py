ac88045b259d1c70e7b1a382e25a0c0ee03001be
nicolamassari <nicola.massari1705@gmail.com>
2024-10-24 17:02:52 +0200

    update predicting.py

--- src/modelling/predicting.py
+++ src/modelling/predicting.py
@@ -2,13 +2,13 @@ import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
 
-from modelling.preprocessing import preprocessing
+from modelling.preprocessing import preprocess_data
 
 
 def predict(df: pd.DataFrame, model: BaseEstimator) -> np.ndarray:
     """Predict Rings based on df data and model"""
 
-    df = preprocessing(df)
+    df = preprocess_data(df)
 
     X = df.drop(columns=["Rings"], errors="ignore")
 
