df8d730b5e4731af52dc856cb0a7edc99277b8fd
茅妍青 <maoyanqing@MacBook-Pro-65.local>
2024-10-24 15:56:08 +0200

    Updated utils.py with improved loading functions

--- src/modelling/utils.py
+++ src/modelling/utils.py
@@ -1,1 +1,20 @@
 # Use this module to code a `pickle_object` function. This will be useful to pickle the model (and encoder if need be).
+import os
+import pickle
+from functools import lru_cache
+
+from loguru import logger
+
+
+@lru_cache
+def load_preprocessor(filepath: os.PathLike):
+    logger.info(f"Loading preprocessor from {filepath}")
+    with open(filepath, "rb") as f:
+        return pickle.load(f)
+
+
+@lru_cache
+def load_model(filepath: os.PathLike):
+    logger.info(f"Loading model from {filepath}")
+    with open(filepath, "rb") as f:
+        return pickle.load(f)
