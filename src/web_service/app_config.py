976903c60857a9e208dbe7e2a57ca2f63d924f56
JACK <jackkhoueiri1@gmail.com>
2024-10-24 18:54:24 +0200

    added app_config

--- src/web_service/app_config.py
+++ src/web_service/app_config.py
@@ -4,11 +4,9 @@
 MODEL_VERSION = "0.0.1"
 PATH_TO_PREPROCESSOR = f"local_models/preprocessor_v{MODEL_VERSION}.pkl"
 PATH_TO_MODEL = f"local_models/model_v{MODEL_VERSION}.pkl"
-CATEGORICAL_VARS = ["sex"]  # Assuming 'sex' is a categorical variable in the dataset
+CATEGORICAL_VARS = ["sex"] 
 
 # MISC
 APP_TITLE = "AbaloneAgePredictionApp"
-APP_DESCRIPTION = (
-    "An API to predict the age of an Abalone (in terms of rings), given its physical measurements and sex."
-)
+APP_DESCRIPTION = "An API to predict the age of an Abalone (in terms of rings), given its physical measurements and sex."
 APP_VERSION = "0.0.1"
