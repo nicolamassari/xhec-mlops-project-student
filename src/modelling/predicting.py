2a682eb145bd2e1562982432f76687b992e5244f
nicolamassari <nicola.massari1705@gmail.com>
2024-10-24 18:12:11 +0200

    add mse

--- src/modelling/predicting.py
+++ src/modelling/predicting.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
-from sklearn.base import BaseEstimator
 from prefect import task
+from sklearn.base import BaseEstimator
+from sklearn.metrics import mean_squared_error
 
 from modelling.preprocessing import preprocess_data
 
@@ -18,3 +19,9 @@ def predict(df: pd.DataFrame, model: BaseEstimator) -> np.ndarray:
     y_pred = model.predict(X)
 
     return y_pred
+
+
+@task(name="Evaluate model")
+def evaluate_model(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """Calculate mean squared error for two arrays"""
+    return mean_squared_error(y_true, y_pred, squared=False)
